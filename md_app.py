205243707dc4cb7c38396b71db07a8849e363417
peter.hsiao <peter.hsiao@deltaww.com>
2024-01-04 21:27:17 +0800

    1. Add video list 2. make upload.html CSS

--- md_app.py
+++ md_app.py
@@ -1,12 +1,14 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 import waitress
 import logging
 import utility.log_decorator as log
 from utility.json_operator import Operator
 from werkzeug.utils import secure_filename
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config['UPLOAD_FOLDER'] = 'D://'
 
 @app.route('/')
@@ -22,6 +24,33 @@ def upload_file():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         return 'Upload Successful!'
     return render_template('upload.html')
+@app.route('/video_list/<int:id>', methods=["GET"])
+def video(id):
+    return get_video_list(id)
+
+@app.route('/video_list', methods=['GET'])
+def video_all():
+    return get_video_list()
+
+def get_video_list(id=0):
+    # Get the directory path from the query parameter 'directory'
+    directory_path = request.args.get('dir')
+
+    if directory_path:
+        # Check if the specified directory exists
+        if os.path.exists(directory_path) and os.path.isdir(directory_path):
+            # Get a list of all files in the directory
+            file_list = os.listdir(directory_path)
+            print(file_list)
+            # Return the file list as JSON
+            if (id == 0):
+                return jsonify(file_list)
+            else:
+                return jsonify(file_list[id])
+        else:
+            return jsonify({"error": "Directory not found."}), 404
+    else:
+        return jsonify({"error": "Missing 'directory' query parameter."}), 400
 
 @log.log_exception
 def main():
